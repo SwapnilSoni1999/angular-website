62b94b4eb6e9591bac846b5973bc6e2fc110a45a
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2019-11-28 15:01:33 +0530

    ads

--- src/app/download-index/download-index.component.ts
+++ src/app/download-index/download-index.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-download-index',
   templateUrl: './download-index.component.html',
   styleUrls: ['./download-index.component.css']
 })
-export class DownloadIndexComponent implements OnInit {
-
+export class DownloadIndexComponent implements AfterViewInit {
+'//p374858.clksite.com/adServe/banners?tid=374858_735346_0';
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    const element = document.createElement('script');
+    element.src = 'blabla';
+    element.type = 'text/javascript';
+    let kek = 
+    document.getElementById('ad').appendChild(element);
   }
-
 }
